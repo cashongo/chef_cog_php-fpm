e78461277b262d2545a0cb5b2e86317f7f09a4ac
Andreas Wagner <whysthatso@gmx.de>
2015-01-20 10:56:36 +0200

    version bump due to uncertain origin of 0.1.1 on node

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
-name 			 "cog_php-fpm"
+name             "cog_php-fpm"
 maintainer       "Cash on Go Ltd"
 maintainer_email "andreas.wagner@cashongo.co.uk"
 license          "Apache 2.0"
 description      "Install and configure php-fpm and pools"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.7"
+version          "0.1.7"
